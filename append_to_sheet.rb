7b09b70845df8911d374a698af5e9aa77dea934c
EvanR <evanrinehart@gmail.com>
2018-03-30 18:46:11 -0500

    uhg

--- append_to_sheet.rb
+++ append_to_sheet.rb
@@ -1,12 +1,12 @@
-require 'lib'
+require_relative 'lib'
 
 input = $stdin.read
 stuff = JSON.parse input
 raise "input not a json object" unless stuff.is_a? Hash
 sheet_id = stuff['sheet_id']
-rows = stuff['rows']
+row = stuff['row']
 raise "sheet_id not provided" if sheet_id.nil?
 raise "sheet_id not a string" unless sheet_id.is_a? String
-raise "rows not provided" if rows.nil?
-raise "rows not an array" unless rows.is_a? Array
-append(sheet_id, rows)
+raise "row not provided" if row.nil?
+raise "row not an array" unless row.is_a? Array
+append(sheet_id, row)
--- lib.rb
+++ lib.rb
@@ -19,16 +19,17 @@ def authorize
   user_id = 'default'
   credentials = authorizer.get_credentials(user_id)
   if credentials.nil?
-    url = authorizer.get_authorization_url( base_url: OOB_URI)
-    puts "Open the following URL in the browser and enter the " +
-         "resulting code after authorization"
-    puts url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id,
-      code: code,
-      base_url: OOB_URI
-    )
+    raise "credentials nil"
+#    url = authorizer.get_authorization_url( base_url: OOB_URI)
+#    puts "Open the following URL in the browser and enter the " +
+#         "resulting code after authorization"
+#    puts url
+#    code = gets
+#    credentials = authorizer.get_and_store_credentials_from_code(
+#      user_id: user_id,
+#      code: code,
+#      base_url: OOB_URI
+#    )
   end
   credentials
 end
@@ -41,7 +42,7 @@ def append sheet_id, row
   service.append_spreadsheet_value(
     sheet_id,
     'A1',
-    {'values': [row]},
+    {values: [row]},
     value_input_option: 'RAW'
   )
 end
